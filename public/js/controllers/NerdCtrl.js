7252e4e3608181e0ebfb3ca576335f2245026bcb
Cam <EMAIL>
2017-03-14 11:25:42 +1300

    use service

--- public/js/controllers/NerdCtrl.js
+++ public/js/controllers/NerdCtrl.js
@@ -1,11 +1,11 @@
-angular.module('NerdCtrl', []).controller('NerdController', function($scope, $http) {
+angular.module('NerdCtrl', []).controller('NerdController', function($scope, Nerd) {
 
     $scope.tagline = 'Nothing beats a pocket protector!';
 
     $scope.formData = {};
 
     // when landing on the page get and show all the nerds
-    $http.get('/api/nerds')
+    Nerd.get()
       .success(function(data) {
         $scope.nerds = data;
         console.log(data);
@@ -14,30 +14,30 @@ angular.module('NerdCtrl', []).controller('NerdController', function($scope, $ht
         console.log('Error: ' + data)
       });
 
-      // submit the form to the API, then repopulate the list
-      $scope.createNerd = function() {
-        $http.post('/api/nerds', $scope.formData)
-          .success(function(data) {
-            // clear the form
-            $scope.formData = {};
-            $scope.nerds = data;
-            console.log(data);
-          })
-          .error(function(data) {
-            console.log('Error: ' + data);
-          });
-      };
+    // submit the form to the API, then repopulate the list
+    $scope.createNerd = function() {
+      Nerd.create($scope.formData)
+        .success(function(data) {
+          // clear the form
+          $scope.formData = {};
+          $scope.nerds = data;
+          console.log(data);
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
+    };
 
-      // delete the nerd, then repopulate the list
-      $scope.deleteNerd = function(id) {
-        $http.delete('/api/nerds/' + id)
-          .success(function(data){
-            $scope.nerds = data;
-            console.log(data);
-          })
-          .error(function(data){
-            console.log('Error: ' + data)
-          })
-      };
+    // delete the nerd, then repopulate the list
+    $scope.deleteNerd = function(id) {
+      Nerd.delete(id)
+        .success(function(data){
+          $scope.nerds = data;
+          console.log(data);
+        })
+        .error(function(data){
+          console.log('Error: ' + data)
+        })
+    };
 
 });
