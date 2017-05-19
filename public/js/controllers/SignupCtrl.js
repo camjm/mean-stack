68d0849dea8e7951b57e2634f48645f0b402b24e
Cam <cam@mackay.net.nz>
2017-05-20 11:53:43 +1200

    signup form

--- public/js/controllers/SignupCtrl.js
+++ public/js/controllers/SignupCtrl.js
@@ -3,21 +3,30 @@ angular.module('meanApp').controller('SignupController',['$scope', '$rootScope',
   $scope.formData = {};
   $scope.message = null;
 
+  $scope.resetForm = function(form) {
+    if (form) {
+      form.$setPristine();
+      form.$setUntouched();
+    }
+    $scope.formData = {};
+  };
+
   // submit the form (all authentication logic in the service)
   $scope.signup = function(credentials, form) {
 
+    if (form.$invalid) return;
+
     AuthService.signup(credentials).then(function(user) {
       // signup successful
-      form.$setPristine();
-      $scope.formData = {};
+      $scope.resetForm(form);
       $scope.message = null;
       $scope.setCurrentUser(user);
       // notify the entire app with broadcast
       $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
     }, function(message) {
       // signup failed
-      form.$setPristine();
-      $scope.formData = {};
+      //form.$setPristine();
+      $scope.resetForm(form);
       $scope.message = message;
       // notify the entire app with broadcast
       $rootScope.$broadcast(AUTH_EVENTS.loginFailure);
---
+++ public/js/directives/Username.js
@@ -0,0 +1,27 @@
+// Simulates validating username against backend API
+
+angular.module('meanApp').directive('username', ['$q', '$timeout', function($q, $timeout) {
+
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      var usernames = ['Cam', 'Jenny', 'Pixel'];
+      ctrl.$asyncValidators.username = function(modelValue, viewValue) {
+        if (ctrl.$isEmpty(modelValue)) {
+          return $q.resolve();
+        }
+        var def = $q.defer();
+        $timeout(function(){
+          if (usernames.indexOf(modelValue) === -1) {
+            def.resolve();
+          } else {
+            def.reject();
+          }
+        }, 2000);
+        return def.promise;
+      };
+    }
+  };
+
+}]);
